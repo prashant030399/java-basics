df6a29ac8922d6744121dd6ac9384581f92f38a0
Prashant Dewangan <166482484+prashant030399@users.noreply.github.com>
2024-08-12 12:02:42 +0530

    Add files via upload

---
+++ ashok sir pattern/e2.java
@@ -0,0 +1,15 @@
+public class e2 {
+    public static void main(String[] args) {
+int n=4;
+int number=10;
+        for(int i=1; i<=n ;i++){
+            for(int j=1; j<=i ;j++)     {
+              System.out.print(number + " ");
+              number--;
+            }
+            System.out.println();
+        }
+        
+    }
+    
+}
---
+++ ashok sir pattern/e3.java
@@ -0,0 +1,15 @@
+public class e3 {
+    public static void main(String[] args) {
+int n=4;
+int number=2;
+        for(int i=1; i<=n ;i++){
+            for(int j=1; j<=i ;j++)     {
+              System.out.print(number + " ");
+              number = number +2;
+            }
+            System.out.println();
+        }
+        
+    }
+    
+}

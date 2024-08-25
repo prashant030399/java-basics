226cf928d1d4a3fe25d92fab3bc663b201729e8c
Prashant Dewangan <166482484+prashant030399@users.noreply.github.com>
2024-08-25 12:16:16 +0530

    Add files via upload

---
+++ codeforwin/helloinput.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class helloinput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String  n = sc.next();
+        
+        System.out.println(n);
+        
+    }
+    
+}

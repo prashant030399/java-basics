de4130f3fe4a228dd07a8283e86c94a511d5d0f2
Prashant Dewangan <166482484+prashant030399@users.noreply.github.com>
2024-09-16 12:34:23 +0530

    Add files via upload

---
+++ Strings/practice.java
@@ -0,0 +1,17 @@
+public class practice {
+
+  public static void main(String args []){
+
+String name1 = "Prashant";
+String name2 = "Prashant";
+
+if(name1.compareTo(name2)==0){
+System.out.print("Strings are equal");
+} else {
+  System.out.print("Strings are not equal");
+}
+
+
+  }
+
+}

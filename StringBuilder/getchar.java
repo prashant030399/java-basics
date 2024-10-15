17201d108e85aeef529792f16f86703f32710b6c
Prashant Dewangan <166482484+prashant030399@users.noreply.github.com>
2024-10-15 18:23:36 +0530

    Add files via upload

---
+++ StringBuilder/getchar.java
@@ -0,0 +1,10 @@
+public class getchar {
+
+    public static void main(String[] args) {
+        
+StringBuilder sb = new StringBuilder("Hello, I m Prashant");
+System.out.print(sb.charAt(0));
+
+    }
+    
+}

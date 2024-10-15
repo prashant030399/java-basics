56257301954b1a47bc64f6ad96a727bdf195aedd
Prashant Dewangan <166482484+prashant030399@users.noreply.github.com>
2024-10-15 18:24:40 +0530

    Add files via upload

---
+++ StringBuilder/setchar.java
@@ -0,0 +1,11 @@
+public class setchar {
+
+    public static void main(String[] args) {
+        
+        StringBuilder sb = new StringBuilder("Hello, I m Prashant");
+sb.setCharAt(0,'P');
+System.out.print(sb);
+
+    }
+    
+}

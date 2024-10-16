3e47da5a39634428784b14285849b50672f14cac
Prashant Dewangan <166482484+prashant030399@users.noreply.github.com>
2024-10-16 11:47:09 +0530

    Add files via upload

---
+++ StringBuilder/appendchar.java
@@ -0,0 +1,11 @@
+public class appendchar {
+
+    public static void main(String[] args) {
+        
+        StringBuilder sb = new StringBuilder("Hello, I m Prashant");
+sb.append(" ");
+        sb.append("Dewangan");
+System.out.print(sb);
+
+
+    }}

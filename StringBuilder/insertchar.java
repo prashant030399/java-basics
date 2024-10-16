6e1f8e24e8915e5c8f92012eca540479a4747660
Prashant Dewangan <166482484+prashant030399@users.noreply.github.com>
2024-10-16 11:44:04 +0530

    Add files via upload

---
+++ StringBuilder/insertchar.java
@@ -0,0 +1,17 @@
+public class insertchar {
+
+    public static void main(String[] args) {
+        
+        StringBuilder sb = new StringBuilder("Hello I m Prashant");
+sb.insert(6,'T');
+sb.insert(7,'h');
+sb.insert(8,'e');
+sb.insert(9,'r');
+sb.insert(10,'e');
+sb.insert(11,' ');
+
+System.out.print(sb);
+
+
+
+    }}

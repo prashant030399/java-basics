9b727f567f03a6055d0689e921c744ee8d364221
Prashant Dewangan <166482484+prashant030399@users.noreply.github.com>
2024-10-16 11:44:54 +0530

    Add files via upload

---
+++ StringBuilder/deletechar.java
@@ -0,0 +1,19 @@
+ public class deletechar {
+
+    public static void main(String[] args) {
+        
+        StringBuilder sb = new StringBuilder("Hello, I m Prashant"); 
+
+    sb.insert(0,'P');
+    System.out.println(sb);
+
+
+    sb.delete(0,1);
+    System.out.print(sb);
+
+
+
+}
+
+
+    }

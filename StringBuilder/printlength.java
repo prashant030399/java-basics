a4a6da703a3187a93a03184e89786f6d4da90a54
Prashant Dewangan <166482484+prashant030399@users.noreply.github.com>
2024-10-16 11:46:18 +0530

    Add files via upload

---
+++ StringBuilder/printlength.java
@@ -0,0 +1,16 @@
+public class printlength {
+
+    public static void main(String[] args) {
+        
+        StringBuilder sb = new StringBuilder("Hello, I m Prashant");
+sb.append(" ");
+sb.append("Dewangan");
+System.out.println(sb);
+System.out.print(sb.length());
+
+
+
+    }
+
+
+}

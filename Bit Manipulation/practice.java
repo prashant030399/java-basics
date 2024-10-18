d04385ab19968d55e73831cc2ef19e51fb1387c6
Prashant Dewangan <166482484+prashant030399@users.noreply.github.com>
2024-10-18 12:08:19 +0530

    Add files via upload

---
+++ Bit Manipulation/practice.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class practice{
+public static void main(String[] args) {
+    
+int n =5;  //0101
+int pos = 3;
+int bitmask = 1<<pos;
+
+if((bitmask & n) == 0){               
+    System.out.print("bit was zero");
+} else {
+System.out.print("bit was one ");
+}
+
+}
+
+}

ee904f611090651d1d0803ce65369cc203e31542
Prashant Dewangan <166482484+prashant030399@users.noreply.github.com>
2024-10-19 12:08:12 +0530

    Add files via upload

---
+++ Bit Manipulation/assignment/problem4.java
@@ -0,0 +1,16 @@
+//4. Write 2 functions => decimalToBinary() & binaryToDecimal() to convert
+//a number from one number system to another. [BONUS]
+import java.util.*;
+
+public class problem4 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+
+        
+    }
+
+    
+}

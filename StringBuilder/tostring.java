0a2de3432f89b8066d360c0a36e19a66a287b5df
Prashant Dewangan <166482484+prashant030399@users.noreply.github.com>
2024-10-21 18:35:54 +0530

    Add files via upload

---
+++ StringBuilder/tostring.java
@@ -0,0 +1,18 @@
+public class tostring {
+
+    public static void main(String[] args) {
+        
+       StringBuilder sb = new StringBuilder();
+       
+       sb.append("Learning ");
+       sb.append("Java ");
+       sb.append("with StringBuilder.");
+
+       String result = sb.toString();
+
+       System.out.print(result);
+
+
+    }
+    
+}
